0ed7af356e70f3691b1b09583898184715367204
Alexander <nefest71@gmail.com>
2023-09-02 17:26:13 +0300

    OnboardSDK init

---
+++ SDK/OnboardSDK/OnboardSDK/OnboardSDK.h
@@ -0,0 +1,18 @@
+//
+//  OnboardSDK.h
+//  OnboardSDK
+//
+//  Created by Alexander V. on 02.09.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OnboardSDK.
+FOUNDATION_EXPORT double OnboardSDKVersionNumber;
+
+//! Project version string for OnboardSDK.
+FOUNDATION_EXPORT const unsigned char OnboardSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OnboardSDK/PublicHeader.h>
+
+
