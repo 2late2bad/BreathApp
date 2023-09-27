eb774c053abf0558368268fcb3abcdea4fa5f3e0
Alexander <nefest71@gmail.com>
2023-09-27 13:50:25 +0300

    Added dependencies of the UIComponents framework with the main module

---
+++ Breath/Setup/UIComponentsFramework.swift
@@ -0,0 +1,14 @@
+//
+//  UIComponentsFramework.swift
+//  Breath
+//
+//  Created by Alexander V. on 27.09.2023.
+//
+
+import UIComponents
+
+final class UIComponentsFramework {
+    static func setup() {
+        UIComponents.Dependencies.setup()
+    }
+}
